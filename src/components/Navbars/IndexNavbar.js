4b7138ed3b7568267f1b883db090e2ca40c80c0e
Bigandy4climate <samuelnihoul@hotmail.fr>
2022-04-22 22:21:49 +0200

    fixed link

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -126,7 +126,7 @@ function IndexNavbar() {
               <Button
                 className="btn-round"
                 color="danger"
-                href="https://www.creative-tim.com/product/paper-kit-pro-react?ref=pkr-index-navbar"
+                href="#"
                 target="_blank"
               >
                 <i className="nc-icon nc-spaceship"></i> coming soon
